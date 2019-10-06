edc4961de20f8ac65b2b1e7b14586a80265df746
Sharuru <mave@foxmail.com>
2019-10-06 20:29:31 +0800

    Frontend basically usable

--- frontend/src/main/java/me/sharuru/jchv/frontend/service/MetaDataService.java
+++ frontend/src/main/java/me/sharuru/jchv/frontend/service/MetaDataService.java
@@ -4,6 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import me.sharuru.jchv.frontend.entity.TblMetaData;
 import me.sharuru.jchv.frontend.model.Node;
 import me.sharuru.jchv.frontend.model.SearchResponse;
+import me.sharuru.jchv.frontend.model.TreantNode;
+import me.sharuru.jchv.frontend.model.TreantRoot;
 import me.sharuru.jchv.frontend.repository.MetaDataRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,11 +21,10 @@ public class MetaDataService {
     @Autowired
     MetaDataRepository metaDataRepository;
 
-
     public SearchResponse search(String searchPath) {
         SearchResponse response = new SearchResponse();
 
-        List<TblMetaData> selfMetaLst = metaDataRepository.findSelfByPath(getSearchPath(searchPath));
+        List<TblMetaData> selfMetaLst = metaDataRepository.findSelfByPath(searchPath);
         if (selfMetaLst.size() > 1) {
             throw new RuntimeException("Find more than one root node.");
         } else if (selfMetaLst.isEmpty()) {
@@ -32,19 +33,44 @@ public class MetaDataService {
         TblMetaData rootNode = selfMetaLst.get(0);
 
         Node<TblMetaData> callerTreeGraph = new Node<>(rootNode);
-        metaDataRepository.findCallerByPath(getSearchPath(rootNode.getMethod())).forEach(childNode -> callerTreeGraph.addChild(new Node<>(childNode)));
+        for (TblMetaData childNode : metaDataRepository.findCallerByPath(rootNode.getMethod())) {
+            callerTreeGraph.addChild(new Node<>(childNode));
+        }
 
         visitChildNode(callerTreeGraph.getChildren());
 
-
-
-
         // TODO
-        response.setCalleeLst(Arrays.asList(rootNode.getContext(), rootNode.getComment()));
-        response.setCallerLst(Arrays.asList(callerTreeGraph.toString()));
+        TreantRoot rootJson = new TreantRoot();
+        TreantNode rootNodeJson = new TreantNode();
+        rootNodeJson.setName(getSearchPath(rootNode.getMethod()));
+        rootNodeJson.setDesc(rootNode.getComment().split("\\R", 2)[0]);
+        rootJson.setText(rootNodeJson);
+        setJson(callerTreeGraph.getChildren(), rootJson);
 
+        response.setCalleeLst(Arrays.asList(rootNode.getContext(), rootNode.getComment().split("\\R", 2)[0]));
+        //response.setCallerLst(Arrays.asList(callerTreeGraph.toString()));
+        response.setNodeStructure(rootJson);
         return response;
 
+    }
+
+
+    private void setJson(List<Node<TblMetaData>> subNode, TreantRoot rootJson){
+        for(Node<TblMetaData> ssNode : subNode){
+            TreantRoot cRoot = new TreantRoot();
+            TreantNode cNode = new TreantNode();
+            cNode.setName(getSearchPath(ssNode.getData().getMethod()));
+            // get first line
+            cNode.setDesc(ssNode.getData().getComment().split("\\R", 2)[0]);
+//            try {
+//                cNode.setDesc(URLEncoder.encode(ssNode.getData().getComment().split("\\R", 2)[0], "utf-8"));
+//            } catch (UnsupportedEncodingException e) {
+//                e.printStackTrace();
+//            }
+            cRoot.setText(cNode);
+            rootJson.getChildren().add(cRoot);
+            setJson(ssNode.getChildren(), cRoot);
+        }
     }
 
     private String getSearchPath(String rawInput) {
@@ -53,7 +79,7 @@ public class MetaDataService {
 
     private void visitChildNode(List<Node<TblMetaData>> parentNodeLst) {
         for (Node<TblMetaData> childNode : parentNodeLst) {
-            for (TblMetaData tblMetaData : metaDataRepository.findCallerByPath(getSearchPath(childNode.getData().getMethod()))) {
+            for (TblMetaData tblMetaData : metaDataRepository.findCallerByPath(childNode.getData().getMethod())) {
                 if (!tblMetaData.getId().equals(childNode.getData().getId())) {
                     if(!isLooping(childNode, tblMetaData)){
                         childNode.addChild(new Node<>(tblMetaData));
@@ -66,6 +92,7 @@ public class MetaDataService {
         }
     }
 
+
     private boolean isLooping(Node<TblMetaData> childNode, TblMetaData tbl) {
         List<Long> pIds = new ArrayList<>();
         Node<TblMetaData> currNode = childNode;
@@ -75,7 +102,7 @@ public class MetaDataService {
         }
         log.info("Current childNodeId: {}", childNode.getData().getId());
         log.info("Current addedNode: {}, Pid: {}, isContain? {}", tbl.getId(), pIds.toString(), pIds.contains(tbl.getId()));
-        tbl.setComment(tbl.getComment() + "#" + tbl.getId() + "#" + pIds.toString() + "#" + pIds.contains(tbl.getId()));
+        //tbl.setComment(tbl.getComment() + "#" + tbl.getId() + "#" + pIds.toString() + "#" + pIds.contains(tbl.getId()));
         if (pIds.contains(tbl.getId())) {
             return true;
         }
--- frontend/src/main/resources/static/page.js
+++ frontend/src/main/resources/static/page.js
@@ -1,6 +1,30 @@
+var chartConfig = {
+    chart: {
+        container: "#tree-graph",
+        rootOrientation: "EAST",
+        connectors: {
+            type: "step",
+            style: {
+                "stroke-width": 2,
+                "stroke": "#ccc"
+            }
+        },
+        node: {
+            collapsable: false,
+            HTMLclass: 'nodeStyle'
+        },
+        levelSeparation: 60,
+        siblingSeparation: 60,
+        subTeeSeparation: 60
+    },
+    nodeStructure: {}
+};
+
 $("#search-button").on("click", function () {
-    var value = $("#text").val();
+    $("#pills-caller-tab").click();
+    var value = $("#text").val().trim();
     console.log("Value: " + value);
+    $("#tree-graph").html("Searching...");
     $.ajax({
             url: "/search",
             type: "get",
@@ -11,17 +35,16 @@ $("#search-button").on("click", function () {
                 },
             success: function (data) {
                 console.log(data);
-                $("#list1").innerHTML += "";
-                var iHtml = "";
-                data.callerLst.forEach(function (data) {
-                    iHtml += "<a href=\"#\" class=\"list-group-item list-group-item-action\">" + data + "</a>";
-                })
-                $("#list1").html(iHtml);
+                chartConfig.nodeStructure = data.nodeStructure;
+                new Treant(chartConfig);
+                $("#calleeLst").html(data.calleeLst[0].replace(/\n/g, "</br>") + "<br/>" + data.calleeLst[1]);
 
             },
             error: function (e) {
                 console.log(e);
+                $("#tree-graph").html("<b>Error: " + e.responseJSON.error + ", " + e.responseJSON.message + "</b><br/>" + e.responseJSON.trace);
             }
         }
     )
 });
+
